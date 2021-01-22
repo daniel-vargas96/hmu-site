de2c833c1a16f184c41f3870e35651ed4ff89185
Daniel Vargas <dannyvargashb@gmail.com>
2021-01-21 20:09:21 -0800

    first half of homepage completed

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,24 +5,14 @@ export default function Home() {
 
   return (
     <>
-      <div className='home container-fluid'>
-          <div className='col'>
-            <div className='title'>
-              <h1>Robinson Authorized Distributor and Service Center</h1>
-            </div>
-            <div className='content-text'>
-              <p>Our highly skilled and trained technicians rely on quality and precision to deliver you with exceptional service that keeps you safe. We assure you that our FAA certified technicians complete thorough training before any maintenance.
-                Our goal is to keep your downtime to a minimum, delivering quality maintenance services with fast turn-around times. You rely on your helicopters for transport. Our diverse clientele and experience providing service at our FAA repair station leaves you with complete satisfaction</p>
-            </div>
-            <div className='arrow-down'>
-              <a className='btn btn-lg down-btn'>
-                <FaIcons.FaChevronDown />
-              </a>
-            </div>
-          </div>
-
-        </div>
-
+      <div className="home justify-content-center align-items-center h-100 col-lg-6 col-lg-offset-3">
+        <h1 className="d-flex display-4 justify-content-center text-center">Robinson Authorized Distributor and Service Center</h1>
+        <p className="d-flex lead text-center">Our highly skilled and trained technicians rely on quality and precision to deliver you with exceptional service that keeps you safe. We assure you that our FAA certified technicians complete thorough training before any maintenance.</p>
+        <p className='d-flex lead text-center'>Our goal is to keep your downtime to a minimum, delivering quality maintenance services with fast turn-around times. You rely on your helicopters for transport. Our diverse clientele and experience providing service at our FAA repair station leaves you with complete satisfaction.</p>
+        <p className='d-flex justify-content-center'>
+          <a className="btn btn-primary btn-lg" href="#" role="button"><FaIcons.FaChevronDown /></a>
+        </p>
+      </div>
     </>
   );
 }
