2e46562499a967df591dd3e5d607afcc1abffa2d
Daniel Vargas <dannyvargashb@gmail.com>
2021-01-25 23:14:40 -0800

    footer styling almost completed

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -15,9 +15,24 @@ export default function Footer() {
     <>
       <footer className="footer">
         <div className="container">
+          <div className='row'>
+            <div className='col-lg-10 col-md-6 mb-lg-0 col-lg-offset-6 mt-5'>
+              <ul className="list-unstyled mb-0 d-flex flex-row">
+                {SidebarData.map((item, index) => {
+                  return (
+                    <li key={index} className={item.class}>
+                      <Link to={item.path}>
+                        {item.title}
+                      </Link>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          </div>
           <div className="row">
-            <div className="col-lg-4 col-md-6 mb-lg-0 d-flex align-items-center text-center">
-              <img src='hmu-logo.png' alt="" width="380" className="mb-3"></img>
+            <div className="col-lg-3 col-md-6 mb-lg-0 d-flex align-items-center text-center">
+              <img src='hmu-logo.png' alt="" width="250" className="mb-3"></img>
               <ul className="list-inline mt-4">
                 <li className="list-inline-item"><a href="#" target="_blank" title="twitter"><i className="fa fa-twitter"></i></a></li>
                 <li className="list-inline-item"><a href="#" target="_blank" title="facebook"><i className="fa fa-facebook"></i></a></li>
@@ -26,31 +41,15 @@ export default function Footer() {
                 <li className="list-inline-item"><a href="#" target="_blank" title="vimeo"><i className="fa fa-vimeo"></i></a></li>
               </ul>
             </div>
-            <div className="col-lg-6 col-md-6 mb-lg-0  d-flex align-items-center text-center">
+            <div className="col-lg-9 col-md-6 mb-lg-0  d-flex align-items-center text-center">
               <h1>HELICOPTER MAINTENANCE UNLIMITED</h1>
-            </div>
-            <div className="col-lg-2 col-md-6 mt-4 mb-2 mb-lg-0 pt-4">
-              {/* <h6 className="text-uppercase font-weight-bold mb-4">HMU</h6> */}
-              <ul className="list-unstyled mb-0">
-                {SidebarData.map((item, index) => {
-                  return (
-                    <li key={index} className={item.class}>
-                      <Link to={item.path}>
-                        {item.icon}
-                        <span>{item.title}</span>
-                      </Link>
-                    </li>
-                  );
-                })}
-              </ul>
             </div>
           </div>
         </div>
-
         {/* <!-- Copyrights --> */}
-        <div className="py-4 footer">
+        <div className="py-3">
           <div className="container text-center">
-            <p className="text-muted mb-0 py-2">© 2021 Helicopter Maintenance Unlimited All rights reserved.</p>
+            <p className="mb-0 py-2">© 2021 Helicopter Maintenance Unlimited All rights reserved.</p>
           </div>
         </div>
       </footer>
